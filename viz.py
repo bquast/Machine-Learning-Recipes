65ae6f27bdca21f0013cf303a6eff7608dde1222
Bastiaan Quast <bquast@gmail.com>
2016-04-20 11:52:31 +0200

    tried with printing pdf, pydot not finding GraphViz, tree works well

--- viz.py
+++ viz.py
@@ -1,7 +1,8 @@
 import numpy as np
 from sklearn.datasets import load_iris
-iris = load_iris()
+from sklearn import tree
 
+iris = load_iris()
 test_idx = [0,50,100]
 
 # traiing data
@@ -16,3 +17,22 @@ clf = tree.DecisionTreeClassifier()
 clf.fit(train_data, train_target)
 
 print(test_target)
+print(clf.predict(test_data))
+
+# # viz code
+# from sklearn.externals.six import StringIO
+# import pydot
+# dot_data = StringIO()
+# tree.export_graphviz(clf,
+#                          out_file=dot_data,
+#                          feature_names=iris.feature_names,
+#                          class_names=iris.target_names,
+#                          filled=True, rounded=True,
+#                          special_characters=True)
+#
+# graph = pydot.graph_from_dot_data(dot_data.getvalue())
+# graph.write_pdf("iris.pdf")
+
+print(test_data[1], test_target[1])
+
+print(iris.feature_names, iris.target_names)
